c7cb4435d975f71905d568ebfd58d27f12933694
zhangzf <zhangzf@finchina.com>
2023-02-25 14:57:11 +0800

    vue3

---
+++ 2023/Vue3/vue3-demo/demo01/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <table />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Table from "../components/Table.vue";
+
+@Options({
+  components: { Table },
+})
+export default class HomeView extends Vue {}
+</script>
